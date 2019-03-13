60499e25e0ab9e6dc6b61d9d7e50b68dab2b4e92
gerardDevelop <gerard.develop@gmail.com>
2019-03-13 17:17:38 +0700

    ok

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(bodyParser.json())
@@ -10,16 +11,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+/*
+
+  FOR HTTPS ----------------------------------------------
+
+  const https = require('https');
+  const fs = require('fs');
+
+const httpsOptions = {
+  cert: fs.readFileSync(path.join(_dirname, 'ssl', 'server.crt')),
+  key: fs.readFileSync(path.join(_dirname, 'ssl', 'server.key'))
+};
+
+
+https.createServer(httpsOptions, app)
+  .listen(port, () => {
+    console.log("Listening on port " + port);
+  });
+
+  --------------------------------------------------------
+
+*/
+
 app.get('/other', (req, res) => {
   res.send("hello hello hello");
 }); 
-
+/*
 app.get('*', (req, res) =>{
   //res.sendFile(path.join(__dirname+'/public/index.html'));
   //res.sendFile('public/index.html');
   res.sendFile(path.join(__dirname + 'public/index.html'));
 }); 
-
+*/
 const port = process.env.PORT || 8080;
 app.listen(port);
 
